84a1d74e981537c78e05ebdf8eacdd364db16e11
Aaron Hobson <aaron@ahob85.com>
2021-04-12 11:53:38 -0700

    update

--- lesson5/project1/script.js
+++ lesson5/project1/script.js
@@ -1,11 +1,21 @@
+// Interface elements
+let canvas;
+let textDiv;
+let label;
+let confidence;
+
 let mobilenet;
 let img;
 
 function setup() {
-  createCanvas(640, 480);
-  img = createImg("images/rabbit.png", imageReady);
+  canvas = createCanvas(640, 480);
+  img = createImg("images/guinea-pig.jpg", imageReady);
   img.hide();
-  background(0);
+  textDiv = createDiv();
+  label = createP();
+  label.parent(textDiv);
+  confidence = createP();
+  confidence.parent(textDiv);
   mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
 
@@ -27,9 +37,7 @@ function gotResult(error, results) {
     console.error(error);
   } else {
     console.log(results);
-    let label = results[0].label;
-    let conf = round(results[0].confidence, 2);
-    createP("Label: " + label);
-    createP("Confidence: " + conf);
+    label.html("Label: " + results[0].label);
+    confidence.html("Confidence: " + round(results[0].confidence, 2));
   }
 }
