2fe559c66c6277f7b562b160ea3ccfb814700fc0
Aaron Hobson <aaron@ahob85.com>
2021-04-26 21:43:42 -0700

    cleaned up comments

--- image-classification/image-classification-1/script.js
+++ image-classification/image-classification-1/script.js
@@ -4,7 +4,8 @@
                           Global UI Variables
 
   canvasDiv, textDiv, buttonDiv
-  In the project's HTML, the DIVs that will contain various elements that may be created in setup(). Useful for styling (e.g., keeping them all centered).
+  In the project's HTML, the DIVs that will contain various elements that may
+  be created in setup(). Useful for styling (e.g., keeping them all centered).
 
   canvas
   The p5.js canvas. This is where all the magic happens!
@@ -13,7 +14,8 @@
   This is where you will print any kind of text (e.g., the label of an image).
 
   buttons
-  If included, these are for user interaction (e.g., training a model, inputing data).
+  If included, these are for user interaction (e.g., training a model, inputing
+  data).
 *******************************************************************************/
 
 //let canvasDiv;
@@ -34,7 +36,9 @@
 /******************************************************************************
                                   setup()
 
-  This is a built-in p5.js function that is automatically called when the program starts, just before draw(). This is used for initializing global variables, building the UI, and loading images, video, datal, and models.
+  This is a built-in p5.js function that is automatically called when the
+  program starts, just before draw(). This is used for initializing global
+  variables, building the UI, and loading images, video, datal, and models.
 *******************************************************************************/
 
 function setup() {
@@ -44,7 +48,9 @@ function setup() {
 /******************************************************************************
                                   draw()
 
-  This is a built-in p5.js function that is automatically called in a repeated loop, just after setup(). This is used for handling animations, or running anything over and over again throughout a program.
+  This is a built-in p5.js function that is automatically called in a repeated
+  loop, just after setup(). This is used for handling animations, or running
+  anything over and over again throughout a program.
 *******************************************************************************/
 
 function draw() {
@@ -54,7 +60,8 @@ function draw() {
 /******************************************************************************
                                 imageLoaded()
 
-  A single-line callback that is called after an image has been loaded by loadImage(). Simply draws the image onto the canvas like this:
+  A single-line callback that is called after an image has been loaded by
+  loadImage(). Simply draws the image onto the canvas like this:
 
   image(img, 0, 0, width, height);
 *******************************************************************************/
@@ -66,11 +73,15 @@ function imageLoaded() {
 /******************************************************************************
                           gotResults(error, results)
 
-  This function is a callback for classify(). In other words, after mobilenet has classified the image, it should call this function next.
+  This function is a callback for classify(). In other words, after mobilenet
+  has classified the image, it should call this function next.
 
   parameters
-  - error: If there was an error while running classify(), it should be brought up here and the function shouldn't do anything else.
-  - results: The results of classify(). This will be an object we can use to get some useful information, such as the predicted label of the image, as well as how confident the model is about that assigned label.
+  - error: If there was an error while running classify(), it should be brought
+  up here and the function shouldn't do anything else.
+  - results: The results of classify(). This will be an object we can use to
+  get some useful information, such as the predicted label of the image, as
+  well as how confident the model is about that assigned label.
 *******************************************************************************/
 
 function gotResults(error, results) {
